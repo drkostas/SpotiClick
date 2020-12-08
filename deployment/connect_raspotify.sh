79b81cc1f07db97ce8805fed5456f45a4efc6b2b
Konstantinos Georgiou <georgiou.kostas94@gmail.com>
2020-12-08 20:11:09 +0200

    Added bash script for the new spotify connect script

---
+++ deployment/connect_raspotify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ..
+source venv/bin/activate
+set -o allexport
+source .env
+set +o allexport
+python spotify_connect.py -c confs/raspotify_connect_conf.yml -l logs/raspotify_connect.log
